784906990146a25d0535175b649f8ac0cf271e7b
salman-ibne-shofi <salmanibnshafi@gmail.com>
2024-02-19 14:41:31 +0600

    keyboard setup and call alphabet

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,3 +1,23 @@
+// function play() {
+// step-1: hide the home scren. to hide the screen add class hidden to the home section
+// const homeSection = document.getElementById("home-screen");
+// homeSection.classList.add("hidden");
+// show the play ground
+
+// show the playground
+// const playgroundSection = document.getElementById("play-ground");
+// playgroundSection.classList.remove('hidden');
+// console.log(playgroundSection.classList)
+// }
+
+function continueGame(){
+    // step-1: generate a random alphabet
+    const alphabet = getArandomAlphabet();
+    console.log('your random alphabet', alphabet);
+}
+
 function play() {
-	console.log("play start now");
+	hideElementById("home-screen");
+    showElementById('play-ground');
+    continueGame()
 }
